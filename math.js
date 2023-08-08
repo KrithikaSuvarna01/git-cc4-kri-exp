e3b0b0055a822a17f140d7de67e8c92ba8138c49
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-08 16:22:46 +0530

    Multiply feature added

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 //This file contans math calculations like addition subtraction multiplication, division and trignometry
+
+/**
+ * 
+ * @param {*} a multiplies 2 numbers
+ * @param {*} b 
+ * @returns a result
+ */
+function mul(a,b){
+    return a*b
+}
