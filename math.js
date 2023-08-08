47e9465d0fe3b757b846ab12db27e07e3f737599
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-08 15:57:18 +0530

    Feat: Addition feature added

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 //This file contans math calculations like addition subtraction multiplication, division and trignometry
+
+/**
+ * 
+ * @param {*} a adds 2 numbers
+ * @param {*} b 
+ * @returns a result
+ */
+function add(a,b){
+    return a+b
+}
