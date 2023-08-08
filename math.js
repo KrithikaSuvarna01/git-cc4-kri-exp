6ffa3147b3e6f16e2c1d8228fa2445515d373457
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-08 16:28:30 +0530

    Feat: Division feature added

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 //This file contans math calculations like addition subtraction multiplication, division and trignometry
+
+/**
+ * This function will return a/b
+ * @param {*} a 
+ * @param {*} b 
+ * @returns a value
+ */
+function div(a,b){
+    return a/b;
+}
